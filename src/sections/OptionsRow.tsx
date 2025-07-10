32542d5fc04336e4e5501317aa48b1babdec86c4
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-07-10 11:15:34 +0530

    added OptionsRow (uninteractive)

--- src/sections/OptionsRow.tsx
+++ src/sections/OptionsRow.tsx
@@ -8,20 +8,54 @@ export function OptionsRow() {
 }
 function LeftOptions() {
     return (
-        <div className="flex items-center gap-2">
-            <div className="flex items-center gap-2 text-sm text-gray-700">
+        <div className="flex items-center gap-4">
+            <div className="flex items-center gap-[4px] text-sm p-[8px] rounded-[4px] bg-gray-100">
                 Tool bar
-                <img src="icons/ChevronDouble.svg" alt="" />
+                <img className="h-[16px] w-[16px]" src="icons/ChevronDouble.svg" alt="" />
             </div>
-            <button className="text-sm text-gray-700">Option 2</button>
+            <div className="h-[24px] w-[1px] bg-gray-300"></div>
+            <div className="flex items-center gap-[4px] text-sm p-[8px] rounded-[4px] ">
+                <div className="flex items-center gap-[4px] p-[8px] rounded-[4px] pr-[12px]">
+                    <img className="h-[16px] w-[16px]" src="icons/Eye.svg" alt="" />
+                    <span>Hide Fields</span>
+                </div>
+                <div className="flex items-center gap-[4px] p-[8px] rounded-[4px] pr-[12px]">
+                    <img className="h-[16px] w-[16px]" src="icons/ArrowSort.svg" alt="" />
+                    <span>Sort</span>
+                </div>
+                <div className="flex items-center gap-[4px] p-[8px] rounded-[4px] pr-[12px]">
+                    <img className="h-[16px] w-[16px]" src="icons/Filter.svg" alt="" />
+                    <span>Filter</span>
+                </div>
+                <div className="flex items-center gap-[4px] p-[8px] rounded-[4px] pr-[12px]">
+                    <img className="h-[16px] w-[16px]" src="icons/ArrowAutofit.svg" alt="" />
+                    <span>Cell View</span>
+                </div>
+            </div>
         </div>
     )
 }
 function RightOptions() {
     return (
         <div className="flex items-center gap-2">
-            <button className="text-sm text-gray-700">Option 3</button>
-            <button className="text-sm text-gray-700">Option 4</button>
+            <div className="flex items-center gap-[8px] text-sm p-[8px] rounded-[4px] ">
+                <div className="text-[#545454] flex items-center gap-[4px] p-[8px] rounded-[6px] pr-[12px] border border-gray-300">
+                    <img className="h-[20px] w-[20px]" src="icons/ArrowDownload.svg" alt="" />
+                    <span>Import</span>
+                </div>
+                <div className="text-[#545454] flex items-center gap-[4px] p-[8px] rounded-[6px] pr-[12px] border border-gray-300">
+                    <img className="h-[20px] w-[20px]" src="icons/ArrowUpload.svg" alt="" />
+                    <span>Export</span>
+                </div>
+                <div className="text-[#545454] flex items-center gap-[4px] p-[8px] rounded-[6px] pr-[12px] border border-gray-300">
+                    <img className="h-[20px] w-[20px]" src="icons/Share.svg" alt="" />
+                    <span>Share</span>
+                </div>
+                <div className="bg-[#4B6A4F] text-white flex items-center gap-[4px] py-[8px] rounded-[6px] px-[24px] border border-gray-300">
+                    <img className="h-[20px] w-[20px]" src="icons/ArrowSplit.svg" alt="ArrowSplit" />
+                    <span>New Action</span>
+                </div>
+            </div>
         </div>
     )
 }
